65ca9adb7c817f0693cdc5b1c3841c73b40aeeb0
Yura <105108355+dizer2@users.noreply.github.com>
2022-08-01 09:00:05 +0200

    Add files via upload

---
+++ homevork js3 massive/js/java.js
@@ -0,0 +1,48 @@
+// let array = [`a`, 
+//     `b`,
+//     `c`,
+//     `d`,
+//     `e`,
+//     `f`,
+//     `g`];
+
+let a = parseInt(prompt(`Введи число a`));
+let b = parseInt(prompt(`Введи число b`));
+let c = parseInt(prompt(`Введи число c`));
+let d = parseInt(prompt(`Введи число d`));
+let e = parseInt(prompt(`Введи число e`));
+let f = parseInt(prompt(`Введи число f`));
+let g = parseInt(prompt(`Введи число g`));
+
+let array = [];
+array.push(a);
+array.push(b);
+array.push(c);
+array.push(d);
+array.push(e);
+array.push(f);
+array.push(g);
+// let myNumber = 0;
+// if (array[0] = a ){
+// }
+// if (array[1] = b){
+// }
+// if (array[2] = c){
+// }
+// if (array[3] = d){
+// }
+// if (array[4] = e){
+// }
+// if (array[5] = f){
+// }
+// if (array[6] = g){
+//     console.log(`Створених масивів:`, array);
+// }
+console.log(array);
+console.log(a+b+c+d+e+f+g);
+let n = array.length;
+console.log(`Кількість елементів масиву:`, n)
+
+// let sum = a+b+c+d+e+f+g;
+
+// console.log(`Сума елементів:`, sum);

aa72fad6f85627da9f06dda7ac64294bf37d4804
Igor <105108355+dizer2@users.noreply.github.com>
2022-09-16 15:38:05 +0200

    Add files via upload

---
+++ hemovork 9 js mouseEvents/script.js
@@ -0,0 +1,116 @@
+alert(`Код закоментований :)`)
+
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
+let box4 = document.getElementById('box4');
+
+// Task1
+// box1.onclick = function() {
+// 	alert(`red`)
+// 	document.body.style.backgroundColor = "red";
+
+// }
+
+// box2.onclick = function() {
+// 	alert(`green`)
+// 	document.body.style.backgroundColor = "green";
+
+// }
+
+// box3.onclick = function() {
+// 	alert(`blue`)
+// 	document.body.style.backgroundColor = "blue";
+
+// }
+
+// box4.onclick = function() {
+// 	alert(`yellow`)
+// 	document.body.style.backgroundColor = "yellow";
+
+// }
+
+// Task2
+// box1.onclick = function() {
+// 	box1.style.display = "none"
+// }
+
+// box2.onclick = function() {
+// 	box2.style.display = "none"
+// }
+
+// box3.onclick = function() {
+// 	box3.style.display = "none"
+// }
+
+// box4.onclick = function() {
+// 	box4.style.display = "none"
+// }
+
+// Task3
+
+
+// box1.onclick = function() {
+// 	box1.style.borderRadius = "50%"
+// }
+
+// box2.onclick = function() {
+// 	box2.style.borderRadius = "50%"
+// }
+
+// box3.onclick = function() {
+// 	box3.style.borderRadius = "50%"
+// }
+
+// box4.onclick = function() {
+// 	box4.style.borderRadius = "50%"
+// }
+
+//  Task4
+
+// box1.onclick = function() {
+// 	box1.style.marginBottom = "200px"
+// }
+
+// box2.onclick = function() {
+// 	box2.style.marginBottom = "200px"
+// }
+
+// box3.onclick = function() {
+// 		box3.style.marginBottom = "200px"
+// }
+
+// box4.onclick = function() {
+// 	box4.style.marginBottom = "200px"
+// }
+
+
+//  Task5
+// box1.onclick = function() {
+// 	box1.style.marginBottom = "200px"
+// 	box2.style.marginBottom = "0px"
+// 	box3.style.marginBottom = "0px"
+// 	box4.style.marginBottom = "0px"
+
+// }
+
+// box2.onclick = function() {
+// 	box2.style.marginBottom = "200px"
+// 	box1.style.marginBottom = "0px"
+// 	box3.style.marginBottom = "0px"
+// 	box4.style.marginBottom = "0px"
+// }
+
+// box3.onclick = function() {
+// 		box3.style.marginBottom = "200px"
+// 		box2.style.marginBottom = "0px"
+// 		box1.style.marginBottom = "0px"
+// 		box4.style.marginBottom = "0px"
+// }
+
+// box4.onclick = function() {
+// 	box4.style.marginBottom = "200px"
+// 	box2.style.marginBottom = "0px"
+// 	box3.style.marginBottom = "0px"
+// 	box1.style.marginBottom = "0px"
+// }
